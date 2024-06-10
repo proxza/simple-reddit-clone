60deaa86d9511888086b857507d8985417a3229f
proxza <proxza@gmail.com>
2024-06-10 20:34:42 +1000

    add shadcn and test icons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import SmileIcon from "@/public/logo-name.svg";
+import SmileIconMobile from "@/public/reddit-full.svg";
+
+import Link from "next/link";
+import Image from "next/image";
+
 export function Navbar() {
   return (
-    <div>
-      <h1>Hellow</h1>
-    </div>
+    <nav className="h-[10vh] w-full flex items-center border-b px-5 lg:px-14 justify-between">
+      <Link href="/" className="flex items-center gap-x-3">
+        <Image src={SmileIconMobile} alt="SmileVault Mobile Icon" className="h-10 w-fit" />
+        <Image src={SmileIcon} alt="SmileVault Icon" className="h-9 w-fit hidden lg:block" />
+      </Link>
+    </nav>
   );
 }
