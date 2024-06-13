dfb902c7ed2855bc2e9cbccfc36524b4ec2b9fef
proxza <proxza@gmail.com>
2024-06-13 12:00:04 +1000

    Add dep

---
+++ app/components/SettingsForm.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+
+export function SettingsForm({ username }: { username: string | null | undefined }) {
+  return (
+    <form>
+      <h1 className="text-3xl font-bold tracking-tight">Settings</h1>
+
+      <Separator className="my-4" />
+      <Label className="text-lg">Username: </Label>
+      <p className="text-muted-foreground">Here you can change your username</p>
+
+      <Input defaultValue={username ?? undefined} name="username" required className="mt-2" min={2} maxLength={21} />
+
+      <div className="w-full mt-5 flex gap-x-5 justify-end">
+        <Button variant="secondary" asChild type="button">
+          <Link href="/">Cancel</Link>
+        </Button>
+        <Button variant="destructive">Save</Button>
+      </div>
+    </form>
+  );
+}
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,7 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import prisma from "../lib/db";
 import { redirect } from "next/navigation";
+import { SettingsForm } from "../components/SettingsForm";
 
 async function getData(userId: string) {
   const data = await prisma.user.findUnique({
@@ -25,8 +26,8 @@ export default async function SettingsPage() {
 
   const data = await getData(user.id);
   return (
-    <div>
-      <h1>HELLO {data?.userName}</h1>
+    <div className="max-w-[1000px] mx-auto flex flex-col mt-4">
+      <SettingsForm username={data?.userName} />
     </div>
   );
 }
