ffd96ff3991bec67b43b63fea78c36871c35c69b
proxza <proxza@gmail.com>
2024-06-16 01:30:53 +1000

    upd

--- app/r/[id]/page.tsx
+++ app/r/[id]/page.tsx
@@ -1,7 +1,25 @@
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function SubRedditRoute() {
   return (
-    <div>
-      <h1>Hello from this subreddit</h1>
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4">
+      <div>
+        <h1>Hello from this subred</h1>
+      </div>
+
+      <div>
+        <Card>
+          <div>About Community</div>
+          <div>
+            <div>
+              <Image src={`https://avatar.vercel.sh/proxz`} width={60} height={60} alt="Community Icon" />
+              <Link href="/">r/proxz</Link>
+            </div>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
