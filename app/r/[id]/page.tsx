4a408b303a66a95c84bf1f8c4d44dea7b8fca5d4
proxza <proxza@gmail.com>
2024-06-16 19:28:49 +1000

    Upd

--- app/r/[id]/page.tsx
+++ app/r/[id]/page.tsx
@@ -1,21 +1,42 @@
+import prisma from "@/app/lib/db";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function SubRedditRoute() {
+async function getData(name: string) {
+  const data = await prisma.subreddit.findUnique({
+    where: {
+      name: name,
+    },
+    select: {
+      name: true,
+      createdAt: true,
+      description: true,
+      userId: true,
+    },
+  });
+
+  return data;
+}
+
+export default async function SubRedditRoute({ params }: { params: { id: string } }) {
+  const data = await getData(params.id);
+
   return (
     <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4">
-      <div>
+      <div className="w-[65%] flex flex-col gap-y-5">
         <h1>Hello from this subred</h1>
       </div>
 
-      <div>
+      <div className="w-[35%]">
         <Card>
-          <div>About Community</div>
-          <div>
-            <div>
-              <Image src={`https://avatar.vercel.sh/proxz`} width={60} height={60} alt="Community Icon" />
-              <Link href="/">r/proxz</Link>
+          <div className="bg-muted p-4 font-semibold">About Community</div>
+          <div className="p-4">
+            <div className="flex items-center gap-x-3">
+              <Image src={`https://avatar.vercel.sh/proxz`} width={60} height={60} alt="Community Icon" className="rounded-full h-16 w-16" />
+              <Link href="/" className="font-medium">
+                r/{data?.name}
+              </Link>
             </div>
           </div>
         </Card>
