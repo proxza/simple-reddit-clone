907f5540c46f83a1cbec07f048a18de4b607bba0
proxza <proxza@gmail.com>
2024-06-19 17:49:33 +1000

    small updates

--- app/r/[id]/page.tsx
+++ app/r/[id]/page.tsx
@@ -2,9 +2,12 @@ import { updateSubDescription } from "@/app/actions";
 import { SubDescriptionForm } from "@/app/components/SubDescriptionForm";
 import { SaveButton } from "@/app/components/SubmitButtons";
 import prisma from "@/app/lib/db";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Cake } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -47,6 +50,30 @@ export default async function SubRedditRoute({ params }: { params: { id: string 
               </Link>
             </div>
             {user?.id === data?.userId ? <SubDescriptionForm description={data?.description} subName={params.id} /> : <p className="text-sm font-normal text-secondary-foreground mt-2">{data?.description}</p>}
+
+            <div className="flex items-center gap-x-2 mt-4">
+              <Cake className="h-6 w-6 text-muted-foreground" />
+              <p className="text-muted-foreground font-medium text-sm">
+                Created:
+                {new Date(data?.createdAt as Date).toLocaleDateString("en-us", {
+                  weekday: "long",
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </p>
+            </div>
+
+            {user?.id ? (
+              <div>
+                <Separator className="my-5" />
+                <Button asChild className="rounded-full w-full">
+                  <Link href={`/r/${data?.name}/create`}>Create Post</Link>
+                </Button>
+              </div>
+            ) : (
+              <br />
+            )}
           </div>
         </Card>
       </div>
