8795080155596eb935c2917e7ac51d0d2b4844ee
proxza <proxza@gmail.com>
2024-06-19 18:32:48 +1000

    Start to create func

---
+++ app/r/[id]/create/page.tsx
@@ -0,0 +1,57 @@
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import pfp from "@/public/pfp.png";
+
+const rules = [
+  {
+    id: 1,
+    text: "Remember the human",
+  },
+  {
+    id: 2,
+    text: "Behave like you would in real life",
+  },
+  {
+    id: 3,
+    text: "Look for the original source of content",
+  },
+  {
+    id: 4,
+    text: "Search for duplication before posting",
+  },
+  {
+    id: 5,
+    text: "Read the community guidelines",
+  },
+];
+
+export default function CreatePostRoute() {
+  return (
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4">
+      <div className="w-[65%] flex flex-col gap-y-5">
+        <h1>Hello there!</h1>
+      </div>
+      <div className="w-[35%]">
+        <Card className="flex flex-col p-4">
+          <div className="flex items-center gap-x-2">
+            <Image src={pfp} alt="PFP" className="h-10 w-10" />
+            <h1 className="font-medium">Posting to community</h1>
+          </div>
+          <Separator className="mt-2" />
+
+          <div className="flex flex-col gap-y-5 mt-5">
+            {rules.map((item) => (
+              <div key={item.id}>
+                <p className="text-sm font-medium">
+                  {item.id}. {item.text}
+                </p>
+                <Separator className="mt-2" />
+              </div>
+            ))}
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+}
