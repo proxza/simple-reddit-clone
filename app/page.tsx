32e49520dc2f36abb00fd078da0d62033cc01daa
proxza <proxza@gmail.com>
2024-06-25 13:19:46 +1000

    Start to create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,39 @@
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
+import Banner from "@/public/banner.png";
+import HelloImage from "@/public/hero-image.png";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <h3>HELLO</h3>;
+  return (
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4">
+      <div className="w-[65%] flex flex-col gap-y-5">
+        <h1>Left side</h1>
+      </div>
+      <div className="w-[35%]">
+        <Card>
+          <Image src={Banner} alt="Banner" />
+          <div className="p-2">
+            <div className="flex items-center">
+              <Image src={HelloImage} alt="Hello" className="w-10 h-16 -mt-6" />
+              <h1 className="font-medium pl-3">Home</h1>
+            </div>
+            <p className="text-sm text-muted-foreground pt-2">Your Home Reddit frontpage. Come here to check in with your favorite communities!</p>
+            <Separator className="mt-5" />
+
+            <div className="flex flex-col gap-y-3">
+              <Button asChild variant="secondary">
+                <Link href="/r/proxza/create">Create Post</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/r/create">Create Community</Link>
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 }
